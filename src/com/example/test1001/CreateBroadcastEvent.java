e169829d2dd9258e70f313f6ac0caeab9aead8bb
Martin Quito <mjquito@gmail.com>
2014-05-28 10:28:35 -0600

    small change, just for testing

--- src/com/example/test1001/CreateBroadcastEvent.java
+++ src/com/example/test1001/CreateBroadcastEvent.java
@@ -40,7 +40,7 @@ public class CreateBroadcastEvent extends AsyncTask<String, Void, String>{
 	protected String doInBackground(String... params) {
 		try{
 			String token = fetchToken();
-			mActivity.show("Token fetched");
+			mActivity.show("Token fetchwed");
 			if( token != null){
 				mActivity.show("Token :" + token);
 			}	
